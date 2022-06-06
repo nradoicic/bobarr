33d3932dc4b809103248b543b99bf2afe60580bb
root <root@bobarr.merp.lan>
2022-06-06 19:41:02 +0000

    Fix incorrect torrent issues
    * Remove seed limit
      Bobarr used to have a 5 seeder limit on torrents.  This made it hard
      to find obscure content content.  This commit allows any number of
      seeders greater than zero.
      The mininmum seed threshold should probably be moved to a config value.
    
    * Only send Latin characters to Jackett
      Sometimes, bobarr would find the right movie but jackett would return a
      torrent for a completely different one.  Jackett only plays well with
      Latin characters, so shows like Fauda would break becuase the
      `tvShow.originalTitle` is in Hebrew.
    
      Rather than continue to exclude counties, bobarr now checks whether
      the original title uses Latin characters.
    
    * Convert Cyrillic to Latin characters
      Alphabets like Cyrillic can do a find-and-replace to become Latin.
      This commit tries to convert from Cyrillic if possible.
    
    * Prioritise indexers reverse alphabetically
      Users often prefer torrents from one tracker over another.
      As a first pass, this commit changes the becaviour to exchaust one
      tracker before looking to another one.
      Tracker priority should likely be a config value in the future.
    
    * Add new Season matching patterns
        - `sezona`
        - `e01 - e`

---
+++ scripts/bobarr.dev.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+set -e # exit when error
+
+cat << "EOF"
+
+    /$$                 /$$
+    | $$                | $$
+    | $$$$$$$   /$$$$$$ | $$$$$$$   /$$$$$$   /$$$$$$   /$$$$$$
+    | $$__  $$ /$$__  $$| $$__  $$ |____  $$ /$$__  $$ /$$__  $$
+    | $$  \ $$| $$  \ $$| $$  \ $$  /$$$$$$$| $$  \__/| $$  \__/
+    | $$  | $$| $$  | $$| $$  | $$ /$$__  $$| $$      | $$
+    | $$$$$$$/|  $$$$$$/| $$$$$$$/|  $$$$$$$| $$      | $$
+    |_______/  \______/ |_______/  \_______/|__/      |__/
+
+        https://github.com/iam4x/bobarr
+
+EOF
+
+args=$1
+
+stop_bobarr() {
+  docker compose down --remove-orphans || true
+}
+
+after_start() {
+  echo ""
+  echo "bobarr started correctly, printing bobarr api logs"
+  echo "you can close this and bobarr will continue to run in backgound"
+  echo ""
+  docker compose logs -f api
+}
+
+if [[ $args == 'start' ]]; then
+  stop_bobarr
+  docker compose up --force-recreate -d
+  after_start
+elif [[ $args == 'dev' ]]; then
+  stop_bobarr
+  docker compose -f docker-compose.yml  -f docker-compose.dev.yml up --force-recreate -d
+  after_start
+elif [[ $args == 'start:vpn' ]]; then
+  stop_bobarr
+  docker compose -f docker-compose.yml -f docker-compose.vpn.yml up --force-recreate -d
+  after_start
+elif [[ $args == 'start:wireguard' ]]; then
+  stop_bobarr
+  docker compose -f docker-compose.yml -f docker-compose.wireguard.yml up --force-recreate -d
+  after_start
+elif [[ $args == 'stop' ]]; then
+  stop_bobarr
+  echo ""
+  echo "bobarr correctly stopped"
+elif [[ $args == 'update' ]]; then
+  docker compose pull
+  echo ""
+  echo "bobarr docker images correctly updated, you can now re-start bobarr"
+else
+  echo "unknow command: $args"
+  echo "use [start | start:vpn | start:wireguard | stop | update]"
+fi
